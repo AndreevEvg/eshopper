679a1e9b3be19c289b0cd7e31fa4ff00c4c97e93
Evgeniy Andreev <e.andreev@yandex.ru>
2018-02-03 03:43:14 +0300

    -

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=eshopper',
     'username' => 'root',
-    'password' => '',
+    'password' => 'evg270583',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
---
+++ models/Category.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\db\ActiveRecord;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property int $id
+ * @property int $parent_id
+ * @property string $name
+ * @property string $keywords
+ * @property string $description
+ */
+class Category extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+    
+    public function getProducts()
+    {
+        return $this->hasMany(Product::className(), ['category_id' => 'id']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['parent_id', 'name'], 'required'],
+            [['parent_id'], 'integer'],
+            [['name', 'keywords', 'description'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'parent_id' => 'Parent ID',
+            'name' => 'Name',
+            'keywords' => 'Keywords',
+            'description' => 'Description',
+        ];
+    }
+}
